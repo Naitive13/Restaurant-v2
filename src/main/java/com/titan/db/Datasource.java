ebac6a36a6def276d4691f92bb84582e09ddb2a6
Naitive13 <hei.titan.2@gmail.com>
2025-04-06 17:14:28 +0300

    feat: create datasource

---
+++ src/main/java/com/titan/db/Datasource.java
@@ -0,0 +1,25 @@
+package com.titan.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Datasource {
+  private final String url = System.getenv("DB_URL");
+  private final String port = System.getenv("DB_PORT");
+  private final String database = System.getenv("DB_DATABASE");
+  private final String user = System.getenv("DB_USER");
+  private final String password = System.getenv("DB_PASSWORD");
+  private final String jdbcURL;
+
+  public Datasource() {
+    jdbcURL = "jdbc:postgresql://" + url + ":" + port + "/" + database;
+  }
+
+  public Connection getConnection() {
+    try {
+      return DriverManager.getConnection(jdbcURL, user, password);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
