bcf099ad7ea27f163ce052279fddd4fd355406e7
Naitive13 <hei.titan.2@gmail.com>
2025-04-07 14:39:41 +0300

    test: add unit tests for single dish retrieval and cost calculation

--- src/test/java/DishTest.java
+++ src/test/java/DishTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @TestMethodOrder(OrderAnnotation.class)
@@ -18,7 +19,20 @@ public class DishTest {
   public void get_all_dishes() {
     List<Dish> dishes = subject.getAll(1,10);
     assertFalse(dishes.isEmpty());
-    System.out.println(dishes);
+  }
+
+  @Test
+  @Order(2)
+  public void get_one_dish(){
+    Dish dish = subject.getById(1L);
+    assertEquals("hotdog", dish.getDishName());
+  }
+
+  @Test
+  @Order(3)
+  public void get_ingredients_cost(){
+    Dish dish = subject.getById(1L);
+    assertEquals(5500, dish.getIngredientsCost());
   }
 
 }
