ce4e3e1b4955dae938e3c6052cf06a964be132d5
Naitive13 <hei.titan.2@gmail.com>
2025-04-10 17:05:07 +0300

    feat: add endpoint for GET /ingredients

--- src/main/java/com/titan/controller/endpoint/IngredientRestController.java
+++ src/main/java/com/titan/controller/endpoint/IngredientRestController.java
@@ -1,21 +1,40 @@
 package com.titan.controller.endpoint;
 
+import com.titan.controller.mapper.IngredientRestMapper;
+import com.titan.model.entities.Ingredient;
 import com.titan.model.rest.IngredientRest;
+import com.titan.service.IngredientService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
+@RequiredArgsConstructor
 public class IngredientRestController {
+  private final IngredientService ingredientService;
+  private final IngredientRestMapper ingredientRestMapper;
+
   @GetMapping("/ingredients")
-  public List<IngredientRest> getIngredients(
+  public ResponseEntity<Object> getIngredients(
       @RequestParam(required = false) Double priceMinFilter,
       @RequestParam(required = false) Double priceMaxFilter,
       @RequestParam int page,
       @RequestParam int pageSize) {
 
+    List<Ingredient> ingredients;
 
+    if (priceMinFilter != null || priceMaxFilter != null) {
+      ingredients =
+          ingredientService.getIngredientsByPrices(priceMinFilter, priceMaxFilter, page, pageSize);
+    } else {
+      ingredients = ingredientService.getAllIngredients(page, pageSize);
+    }
+
+    List<IngredientRest> ingredientRests = ingredients.stream().map(ingredientRestMapper::toRest).toList();
+    return ResponseEntity.ok().body(ingredientRests);
   }
 }
