b23c6205729f1a4a5c9c8b37c5f602eafbc4f867
Naitive13 <hei.titan.2@gmail.com>
2025-04-10 14:59:40 +0300

    feat: create a HealthRestController to test the application

---
+++ src/main/java/com/titan/controller/HealthRestController.java
@@ -0,0 +1,12 @@
+package com.titan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthRestController {
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
