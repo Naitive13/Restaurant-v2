13c59b79f624c83cf519784dacd9e30f850d18cf
Naitive13 <hei.titan.2@gmail.com>
2025-04-10 11:45:25 +0300

    feat: update automatically dishOrders to in progress when order is in progress

--- src/main/java/com/titan/entities/Order.java
+++ src/main/java/com/titan/entities/Order.java
@@ -86,6 +86,8 @@ public class Order {
           status.setCreationDate(LocalDateTime.of(2025, 1, 3, 0, 0, 0));
           status.setId((long) status.hashCode());
 
+          this.getDishOrders().forEach(DishOrder::updateStatus);
+
           List<OrderStatus> newStatusList = new ArrayList<>(this.getStatusList());
           newStatusList.add(status);
           this.setStatusList(newStatusList);
