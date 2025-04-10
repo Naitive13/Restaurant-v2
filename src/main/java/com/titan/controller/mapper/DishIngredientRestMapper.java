988bc1b7ebb306b2be922c7cb4ef23fb8b57d405
Naitive13 <hei.titan.2@gmail.com>
2025-04-10 21:39:05 +0300

    feat: create rest mapper for Dish and DishIngredient

---
+++ src/main/java/com/titan/controller/mapper/DishIngredientRestMapper.java
@@ -0,0 +1,22 @@
+package com.titan.controller.mapper;
+
+import com.titan.model.entities.DishIngredient;
+import com.titan.model.rest.DishIngredientRest;
+
+public class DishIngredientRestMapper implements BiMapper<DishIngredientRest, DishIngredient> {
+  @Override
+  public DishIngredientRest toRest(DishIngredient dishIngredient) {
+    DishIngredientRest dishIngredientRest = new DishIngredientRest();
+    dishIngredientRest.setId(dishIngredient.getDishId());
+    dishIngredientRest.setName(dishIngredient.getIngredient().getIngredientName());
+    dishIngredientRest.setRequiredQuantity(dishIngredient.getQuantity());
+    dishIngredientRest.setUnitType(dishIngredient.getIngredient().getUnit());
+
+    return dishIngredientRest;
+  }
+
+  @Override
+  public DishIngredient toModel(DishIngredientRest dishIngredientRest) {
+    return null;
+  }
+}
---
+++ src/main/java/com/titan/controller/mapper/DishRestMapper.java
@@ -0,0 +1,30 @@
+package com.titan.controller.mapper;
+
+import com.titan.model.entities.Dish;
+import com.titan.model.rest.DishRest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DishRestMapper implements BiMapper<DishRest, Dish> {
+  private final DishIngredientRestMapper dishIngredientRestMapper;
+
+  @Override
+  public DishRest toRest(Dish dish) {
+    DishRest dishRest = new DishRest();
+    dishRest.setId(dishRest.getId());
+    dishRest.setName(dish.getDishName());
+    dishRest.setAvailableQuantity(dish.getAvailableQuantity());
+    dishRest.setActualPrice((double) dish.getDishPrice());
+    dishRest.setIngredients(
+        dish.getIngredientList().stream().map(dishIngredientRestMapper::toRest).toList());
+
+    return dishRest;
+  }
+
+  @Override
+  public Dish toModel(DishRest dishRest) {
+    return null;
+  }
+}
