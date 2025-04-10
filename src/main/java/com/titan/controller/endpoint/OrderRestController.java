8d5c2d751695eaf9dd3759a84c51513761daeee2
Naitive13 <hei.titan.2@gmail.com>
2025-04-10 23:51:10 +0300

    feat: implement PUT /order/{references}/dishes

--- src/main/java/com/titan/controller/endpoint/OrderRestController.java
+++ src/main/java/com/titan/controller/endpoint/OrderRestController.java
@@ -1,25 +1,58 @@
 package com.titan.controller.endpoint;
 
+import com.titan.controller.mapper.DishOrderRestMapper;
 import com.titan.controller.mapper.OrderRestMapper;
+import com.titan.model.entities.DishOrder;
 import com.titan.model.entities.Order;
+import com.titan.model.rest.DishOrderRest;
 import com.titan.model.rest.OrderRest;
 import com.titan.service.OrderService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderRestController {
-    private final OrderService orderService;
-    private final OrderRestMapper orderRestMapper;
+  private final OrderService orderService;
+  private final OrderRestMapper orderRestMapper;
+  private final DishOrderRestMapper dishOrderRestMapper;
 
-    @GetMapping("/orders/{reference}")
-    public ResponseEntity<Object> getOrderByReference(@PathVariable String reference){
-        Order order = orderService.getOrderByReference(reference);
-        OrderRest orderRest = orderRestMapper.toRest(order);
-        return ResponseEntity.ok().body(orderRest);
-    }
+  @GetMapping("/orders/{reference}")
+  public ResponseEntity<Object> getOrderByReference(@PathVariable String reference) {
+    Order order = orderService.getOrderByReference(reference);
+    OrderRest orderRest = orderRestMapper.toRest(order);
+    return ResponseEntity.ok().body(orderRest);
+  }
+
+  @PutMapping("/orders/{reference}/dishes")
+  public ResponseEntity<Object> updateDishOrders(
+      @PathVariable String reference, @RequestBody List<DishOrderRest> dishOrderRests) {
+    List<DishOrder> dishOrders =
+        dishOrderRests.stream()
+            .map(
+                dishOrderRest -> {
+                  DishOrder updatedDishOrder = dishOrderRestMapper.toModel(dishOrderRest);
+                  updatedDishOrder.setOrderReference(reference);
+                  DishOrder originalDishOrder =
+                      orderService.getOrderByReference(reference).getDishOrders().stream()
+                          .filter(
+                              item ->
+                                  item.getDish()
+                                      .getDishName()
+                                      .equals(updatedDishOrder.getDish().getDishName()))
+                          .findFirst()
+                          .get();
+                  updatedDishOrder.setId(originalDishOrder.getId());
+                  updatedDishOrder.setStatusList(originalDishOrder.getStatusList());
+                  return updatedDishOrder;
+                })
+            .toList();
+    orderService.updateDishOrders(dishOrders);
+
+    Order order = orderService.getOrderByReference(reference);
+    OrderRest orderRest = orderRestMapper.toRest(order);
+    return ResponseEntity.ok().body(orderRest);
+  }
 }
--- src/main/java/com/titan/service/OrderService.java
+++ src/main/java/com/titan/service/OrderService.java
@@ -1,16 +1,25 @@
 package com.titan.service;
 
+import com.titan.model.entities.DishOrder;
 import com.titan.model.entities.Order;
+import com.titan.repository.dao.DishOrderDAO;
 import com.titan.repository.dao.OrderDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderDAO orderDAO;
+    private final DishOrderDAO dishOrderDAO;
 
     public Order getOrderByReference(String reference){
         return orderDAO.getByReference(reference);
+    }
+
+    public void updateDishOrders(List<DishOrder> dishOrder) {
+        dishOrderDAO.saveAll(dishOrder);
     }
 }
