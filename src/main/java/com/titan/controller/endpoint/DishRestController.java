6bd4b5ca1c47791a096a3a3b57752d6d00250b5e
Naitive13 <hei.titan.2@gmail.com>
2025-04-10 22:12:12 +0300

    feat: implement PUT /dishes/{id}/ingredients

--- src/main/java/com/titan/controller/endpoint/DishRestController.java
+++ src/main/java/com/titan/controller/endpoint/DishRestController.java
@@ -1,27 +1,46 @@
 package com.titan.controller.endpoint;
 
+import com.titan.controller.mapper.DishIngredientRestMapper;
 import com.titan.controller.mapper.DishRestMapper;
 import com.titan.model.entities.Dish;
+import com.titan.model.entities.DishIngredient;
+import com.titan.model.rest.DishIngredientRest;
 import com.titan.model.rest.DishRest;
 import com.titan.service.DishService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class DishRestController {
-    private final DishService dishService;
-    private final DishRestMapper dishRestMapper;
+  private final DishService dishService;
+  private final DishRestMapper dishRestMapper;
+  private final DishIngredientRestMapper dishIngredientRestMapper;
 
-    @GetMapping("/dishes")
-    public ResponseEntity<Object> getAllDishes(@RequestParam int page, @RequestParam int pageSize){
-        List<Dish> dishes = dishService.getAllDishes(page,pageSize);
-        List<DishRest> dishRests = dishes.stream().map(dishRestMapper::toRest).toList();
-        return ResponseEntity.ok().body(dishRests);
-    }
+  @GetMapping("/dishes")
+  public ResponseEntity<Object> getAllDishes(@RequestParam int page, @RequestParam int pageSize) {
+    List<Dish> dishes = dishService.getAllDishes(page, pageSize);
+    List<DishRest> dishRests = dishes.stream().map(dishRestMapper::toRest).toList();
+    return ResponseEntity.ok().body(dishRests);
+  }
+
+  @PutMapping("/dishes/{id}/ingredients")
+  public ResponseEntity<Object> updateDishIngredients(
+      @PathVariable Long id, @RequestBody List<DishIngredientRest> dishIngredientsToAdd) {
+    List<DishIngredient> dishIngredients =
+        dishIngredientsToAdd.stream()
+            .map(
+                dishIngredientRest -> {
+                  DishIngredient dishIngredient =
+                      dishIngredientRestMapper.toModel(dishIngredientRest);
+                  dishIngredient.setDishId(id);
+                  return dishIngredient;
+                })
+            .toList();
+
+    dishService.createOrUpdateDishIngredients(dishIngredients);
+    return ResponseEntity.ok().body(dishIngredientsToAdd);
+  }
 }
--- src/main/java/com/titan/service/DishService.java
+++ src/main/java/com/titan/service/DishService.java
@@ -1,7 +1,9 @@
 package com.titan.service;
 
 import com.titan.model.entities.Dish;
+import com.titan.model.entities.DishIngredient;
 import com.titan.repository.dao.DishDAO;
+import com.titan.repository.dao.DishIngredientDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DishService {
     private final DishDAO dishDAO;
+    private final DishIngredientDAO dishIngredientDAO;
 
     public List<Dish> getAllDishes(int page, int pageSize) {
         return dishDAO.getAll(page,pageSize);
+    }
+
+    public void createOrUpdateDishIngredients(List<DishIngredient> dishIngredients) {
+        dishIngredientDAO.saveAll(dishIngredients);
     }
 }
