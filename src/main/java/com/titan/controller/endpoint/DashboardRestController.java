d9e51053dc4e43bd990b98d88e9576d7283d179c
Naitive13 <hei.titan.2@gmail.com>
2025-04-17 15:06:21 +0300

    feat: implement GET /bestSales

--- src/main/java/com/titan/controller/endpoint/DashboardRestController.java
+++ src/main/java/com/titan/controller/endpoint/DashboardRestController.java
@@ -1,20 +1,43 @@
 package com.titan.controller.endpoint;
 
+import com.titan.model.entities.SoldDish;
+import com.titan.service.DashboardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class DashboardRestController {
-    @GetMapping("/bestSales")
-    public ResponseEntity<Object> getBestSales(){
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-    }
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-    @GetMapping("/dishes/{id}/processingTime")
-    public ResponseEntity<Object> getProcessingTime(@PathVariable Long id){
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+@RestController
+@RequiredArgsConstructor
+public class DashboardRestController {
+  private final DashboardService dashboardService;
+
+  @GetMapping("/bestSales")
+  public ResponseEntity<Object> getBestSales(
+      @RequestParam LocalDateTime dateMin,
+      @RequestParam LocalDateTime dateMax,
+      @RequestParam int size) {
+    try {
+      List<SoldDish> soldDishes = dashboardService.getBestSales(dateMin, dateMax);
+      List<SoldDish> body = new ArrayList<>();
+      while (body.size() != size) {
+        body.add(soldDishes.removeFirst());
+      }
+      return ResponseEntity.ok().body(body);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
+  }
+
+  @GetMapping("/dishes/{id}/processingTime")
+  public ResponseEntity<Object> getProcessingTime(@PathVariable Long id) {
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
 }
---
+++ src/main/java/com/titan/service/DashboardService.java
@@ -0,0 +1,66 @@
+package com.titan.service;
+
+import static com.titan.model.enums.StatusType.DONE;
+
+import com.titan.model.entities.DishOrder;
+import com.titan.model.entities.SoldDish;
+import com.titan.repository.dao.DishOrderDAO;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DashboardService {
+  private final DishOrderDAO dishOrderDAO;
+
+  public List<SoldDish> getBestSales(LocalDateTime dateMin, LocalDateTime dateMax) {
+    List<DishOrder> dishOrders = dishOrderDAO.getAll(1, 100);
+    List<DishOrder> finished =
+        dishOrders.stream()
+            .filter(
+                dishOrder ->
+                    dishOrder.getStatusList().stream()
+                        .anyMatch(dishOrderStatus -> dishOrderStatus.getStatus().equals(DONE)))
+            .toList();
+    List<DishOrder> dishOrdersBetweenInterval =
+        finished.stream()
+            .filter(
+                dishOrder ->
+                    (dishOrder.getActualStatus().getCreationDate().isAfter(dateMin)
+                            && dishOrder.getActualStatus().getCreationDate().isBefore(dateMax)))
+            .toList();
+
+    List<SoldDish> soldDishes = new ArrayList<>();
+    dishOrdersBetweenInterval.forEach(
+        dishOrder -> {
+          if (soldDishes.stream()
+              .anyMatch(
+                  soldDish -> soldDish.getDishName().equals(dishOrder.getDish().getDishName()))) {
+            for (int i = 0; i < soldDishes.size(); i++) {
+              if (soldDishes.get(i).getDishName().equals(dishOrder.getDish().getDishName())) {
+                Double profit = soldDishes.get(i).getTotalProfit();
+                soldDishes.get(i).setTotalProfit(profit + dishOrder.getTotalAmount());
+                Long quantity = soldDishes.get(i).getTotalQuantities();
+                soldDishes.get(i).setTotalQuantities(quantity + dishOrder.getQuantity());
+              }
+            }
+          } else {
+            SoldDish soldDish = new SoldDish();
+
+            soldDish.setDishName(dishOrder.getDish().getDishName());
+            soldDish.setTotalProfit(dishOrder.getTotalAmount().doubleValue());
+            soldDish.setTotalQuantities((long) dishOrder.getQuantity());
+
+            soldDishes.add(soldDish);
+          }
+        });
+
+    return soldDishes.stream()
+        .sorted(Comparator.comparing(SoldDish::getTotalQuantities, Comparator.naturalOrder()))
+        .toList();
+  }
+}
