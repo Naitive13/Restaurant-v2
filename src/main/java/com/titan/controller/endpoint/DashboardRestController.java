7d2a413b36d6e8ba28f489a47f8a57b323e27b2e
Naitive13 <hei.titan.2@gmail.com>
2025-04-17 18:31:11 +0300

    feat: implement GET /dishes/{id}/processingTime

--- src/main/java/com/titan/controller/endpoint/DashboardRestController.java
+++ src/main/java/com/titan/controller/endpoint/DashboardRestController.java
@@ -1,8 +1,16 @@
 package com.titan.controller.endpoint;
 
+import com.titan.model.entities.ProcessingTime;
 import com.titan.model.entities.SoldDish;
+import com.titan.model.enums.DurationType;
+import com.titan.model.enums.ProcessingTimeType;
 import com.titan.service.DashboardService;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +18,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 public class DashboardRestController {
+  private static final Logger log = LoggerFactory.getLogger(DashboardRestController.class);
   private final DashboardService dashboardService;
 
   @GetMapping("/bestSales")
@@ -37,7 +42,24 @@ public class DashboardRestController {
   }
 
   @GetMapping("/dishes/{id}/processingTime")
-  public ResponseEntity<Object> getProcessingTime(@PathVariable Long id) {
-    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  public ResponseEntity<Object> getProcessingTime(
+      @PathVariable Long id,
+      @RequestParam(defaultValue = "SECOND", required = false) String durationType,
+      @RequestParam(defaultValue = "AVERAGE", required = false) String processingTimeType) {
+
+    try {
+      log.info(durationType);
+      log.info(processingTimeType);
+      ProcessingTime processingTime =
+          dashboardService.getProcessingTimeFor(
+              id,
+              ProcessingTimeType.valueOf(processingTimeType),
+              DurationType.valueOf(durationType));
+      return ResponseEntity.ok().body(processingTime);
+
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
   }
 }
--- src/main/java/com/titan/service/DashboardService.java
+++ src/main/java/com/titan/service/DashboardService.java
@@ -1,10 +1,13 @@
 package com.titan.service;
 
 import static com.titan.model.enums.StatusType.DONE;
+import static com.titan.model.enums.StatusType.IN_PROGRESS;
 
-import com.titan.model.entities.DishOrder;
-import com.titan.model.entities.SoldDish;
+import com.titan.model.entities.*;
+import com.titan.model.enums.DurationType;
+import com.titan.model.enums.ProcessingTimeType;
 import com.titan.repository.dao.DishOrderDAO;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -31,7 +34,7 @@ public class DashboardService {
             .filter(
                 dishOrder ->
                     (dishOrder.getActualStatus().getCreationDate().isAfter(dateMin)
-                            && dishOrder.getActualStatus().getCreationDate().isBefore(dateMax)))
+                        && dishOrder.getActualStatus().getCreationDate().isBefore(dateMax)))
             .toList();
 
     List<SoldDish> soldDishes = new ArrayList<>();
@@ -62,5 +65,56 @@ public class DashboardService {
     return soldDishes.stream()
         .sorted(Comparator.comparing(SoldDish::getTotalQuantities, Comparator.naturalOrder()))
         .toList();
+  }
+
+  public ProcessingTime getProcessingTimeFor(
+      Long dishId, ProcessingTimeType processingTimeType, DurationType durationType) {
+    List<DishOrder> dishOrders = dishOrderDAO.getByDishId(dishId);
+    List<Duration> durations =
+        dishOrders.stream()
+            .map(
+                dishOrder -> {
+                  LocalDateTime inProgressDate =
+                      dishOrder.getStatusList().stream()
+                          .filter(
+                              dishOrderStatus -> dishOrderStatus.getStatus().equals(IN_PROGRESS))
+                          .toList()
+                          .getFirst()
+                          .getCreationDate();
+                  LocalDateTime doneDate =
+                      dishOrder.getStatusList().stream()
+                          .filter(dishOrderStatus -> dishOrderStatus.getStatus().equals(DONE))
+                          .toList()
+                          .getFirst()
+                          .getCreationDate();
+                  return Duration.between(inProgressDate, doneDate);
+                })
+            .toList();
+
+    ProcessingTime result = new ProcessingTime();
+    Duration duration = null;
+    result.setProcessingTimeType(processingTimeType);
+    result.setDurationType(durationType);
+    switch (processingTimeType) {
+      case MINIMUM -> {
+        duration = durations.stream().min(Duration::compareTo).get();
+      }
+
+      case MAXIMUM -> {
+        duration = durations.stream().max(Duration::compareTo).get();
+      }
+
+      case AVERAGE -> {
+        duration = durations.stream().reduce((Duration::plus)).get().dividedBy(durations.size());
+      }
+    }
+
+    switch (durationType) {
+      case SECOND -> result.setValue(duration.toSeconds());
+      case MINUTE -> result.setValue(duration.toMinutes());
+      case HOUR -> result.setValue(duration.toHours());
+    }
+
+    return result;
   }
 }
