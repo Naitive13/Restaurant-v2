0453426b3ec52039a40d13841e68df1e08f850cc
Naitive13 <hei.titan.2@gmail.com>
2025-04-18 10:40:55 +0300

    feat: create a dish order rest with a status list

---
+++ src/main/java/com/titan/controller/mapper/DishOrderRestWithStatusListMapper.java
@@ -0,0 +1,26 @@
+package com.titan.controller.mapper;
+
+import com.titan.model.entities.DishOrder;
+import com.titan.model.rest.DishOrderRestWithStatusList;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class DishOrderRestWithStatusListMapper
+    implements Function<DishOrder, DishOrderRestWithStatusList> {
+  @Override
+  public DishOrderRestWithStatusList apply(DishOrder dishOrder) {
+    DishOrderRestWithStatusList item = new DishOrderRestWithStatusList();
+
+    item.setDishOrderId(dishOrder.getId());
+    item.setDishId(dishOrder.getDish().getDishId());
+    item.setStatusList(dishOrder.getStatusList());
+    item.setDishName(dishOrder.getDish().getDishName());
+    item.setActualStatus(dishOrder.getActualStatus());
+    item.setQuantity(dishOrder.getQuantity());
+    item.setTotalAmount(dishOrder.getTotalAmount());
+
+    return item;
+  }
+}
